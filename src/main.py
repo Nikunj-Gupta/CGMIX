0fb29d6a2a1d16530b8a23ecc8d179471911ffa6
Nikunj Gupta <nikunj@usc.edu>
2025-09-17 17:35:07 -0700

    fixed minor bug
    collections.Mapping --> collections.abc.Mapping

--- src/main.py
+++ src/main.py
@@ -55,7 +55,7 @@ def _get_config(params, arg_name, subfolder):
 def recursive_dict_update(d, u):
     for k, v in u.items():
         #print(k, v)
-        if isinstance(v, collections.Mapping):
+        if isinstance(v, collections.abc.Mapping):
             d[k] = recursive_dict_update(d.get(k, {}), v)
         else:
             d[k] = v
